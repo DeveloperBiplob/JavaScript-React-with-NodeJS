d6e719ea771bdfaf9a41df9e4e1a66c0d3f0f9cc
BIPLOB <developerbiplob.bd@gmail.com>
2024-01-02 21:31:18 +0600

    Calculate Facturial of a number Using for Loop

--- JavaScript/basicJs.js
+++ JavaScript/basicJs.js
@@ -1,14 +1,23 @@
 // console.log('Hello')
 // Array index
-var number = [10,20,30];
-var findIndex = number.indexOf(20);
-console.log(findIndex)
+// var number = [10,20,30];
+// var findIndex = number.indexOf(20);
+// console.log(findIndex)
 
-// Array Value Replace Or Set 
- var num = [30,50,80,90];
+// // Array Value Replace Or Set 
+//  var num = [30,50,80,90];
 
-num[1] = 250;
-console.log(num)
+// num[1] = 250;
+// console.log(num)
 
-var name = ['biplob', 'bipu', 'jabery']
-console.log(name[1]);
+// var name = ['biplob', 'bipu', 'jabery']
+// console.log(name[1]);
+
+
+// Calculate Facturial of a number Using for Loop-------------
+// 7! = 7 x 6 x 5 x 4 x 3 x 2 x 1
+let facturial = 1
+for(var i = 1; i <=7; i++){
+    facturial = facturial* i;
+}
+console.log(facturial)
